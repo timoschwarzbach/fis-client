d62aad954fe0ea913fa7442134bbd23605ea938f
timolated <thonphet2312@gmail.com>
2025-02-19 22:42:16 +0100

    remove old screens

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,6 @@ import { ScreenContext, ScreenProvider } from "./provder/Screen";
 import MapView from "./views/Map";
 import Upcoming from "./views/Upcoming";
 import { DevOverlay } from "./views/DevOverlay";
-import ImageView from "./views/Image";
-import NewsView from "./views/News";
-import VideoView from "./views/Video";
 import MediaView from "./views/Media";
 
 function App() {
@@ -29,13 +26,6 @@ function Screens() {
 			<MapView
 				hidden={screen.screen !== "map" && screen.screen !== "mapview"}
 			/>
-
-			{/* deprecated start */}
-			<ImageView hidden={screen.screen !== "image"} />
-			<VideoView hidden={screen.screen !== "video"} />
-			<NewsView hidden={screen.screen !== "news"} />
-			{/* deprecated end */}
-
 			<MediaView hidden={screen.screen !== "media"} />
 		</>
 	);
